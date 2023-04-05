06a6627a68ccd1954c6640f53ee96053ea4eae49
Alejandra <124294207+AlBeker@users.noreply.github.com>
2023-04-05 18:46:25 +0200

    Add new game

---
+++ script.js
@@ -0,0 +1,52 @@
+function getComputerChoice() {
+ const choices = ["Rock", "Paper", "Scissors"];
+ Math.floor(Math.random() * choices.length)
+ return choices[randomIndex]
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowercase();
+    computerSelection = computerSelection.toLowercase();
+
+if (playerSelection === computerSelection) {
+ return "It's a tie!"; }
+else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors"; }
+else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats rock"; }
+else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beat paper"; }
+else {
+    return "You lose! " + computerSelection + "beats" + playerSelection;
+}
+}
+
+function game() {
+ let playerScore = 0;
+ let computerScore = 0;
+
+ for (let i=0; i < 5; i++) {
+    const playerSelection = prompt("Choose Rock, Paper, or Scissors:");
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
+    if (result.startsWith("You win!")) {
+        playerScore++;
+    }
+    else if (result.startsWith("You lose")) {
+        computerScore++;
+    }
+console.log(result);
+ }
+
+}
+
+if (playerScore > computerScore) {
+    console.log("You win the game! Final score: " + playerScore + " to " + computerScore);
+ }
+else if (computerScore > playerScore) {
+    console.log("You lose the game! Final score: " + computerScore + " to " + playerScore);   
+}
+else {
+    console.log("It's a tie! Dinal score: " + playerScore + " to " + computerScore);
+}
